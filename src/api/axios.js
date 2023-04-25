8f3507b57bb90c221c137fdd103f446534cf2e44
mmtrez <immtrez@gmail.com>
2023-04-26 02:46:55 +0330

    update api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://testapiiiii.herokuapp.com/api",
+  baseURL: 'https://dorm-managment-api.onrender.com/api/',
 });
